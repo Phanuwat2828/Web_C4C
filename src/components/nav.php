844e037e5305e5a43e9dad50f973e88a9fd2ba8e
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2025-02-10 15:55:10 +0700

    update

---
+++ src/components/nav.php
@@ -0,0 +1,11 @@
+<html>
+
+<head></head>
+
+<body>
+    <header>
+        <h1>WebSite Name</h1>
+    </header>
+    <nav>
+        <a href="/">Home</a>
+    </nav>
---
+++ src/index.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+// Constant values for this project
+const INCLUDES_DIR = __DIR__ . '/includes';
+const ROUTE_DIR = __DIR__ . '/routes';
+const COMPONENTS_DIR = __DIR__ . '/components';
+
+session_start();
+
+// Include router to index.php 
+require_once INCLUDES_DIR . '/router.php';
+require_once INCLUDES_DIR . '/view.php';
+
+// Call dispatch to handle requests
+// echo '$_SERVER["REQUEST_URI"]='.$_SERVER['REQUEST_URI'];
+dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
